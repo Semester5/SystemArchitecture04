0f9c770fb8f650d4f981c3c4d7c69baf044c92e2
tscholy <christina.tschol@students.fhv.at>
2017-12-05 14:11:48 +0100

    first controller done

--- src/AControllerProp.java
+++ src/AControllerProp.java
@@ -26,7 +26,7 @@ public class AControllerProp extends DifferentialWheels {
         super();
         // get distance sensors and save them in array
         sensors = new LightSensor[] { getLightSensor("ls1"), getLightSensor("ls4"), getLightSensor("ls6") };
-        for (int i=0; i<4; i++) {
+        for (int i=0; i<3; i++) {
             sensors[i].enable(10);
         }
     }
@@ -48,23 +48,37 @@ public class AControllerProp extends DifferentialWheels {
      */
     public void run() {
         while (step(TIME_STEP) != -1) {
-
-            double sensorarray[][] = {};
+            double sensorarray[][] = new double[3][1];
             for(int i = 0; i < sensors.length; i++) {
-                sensorarray[i][0] = sensors[i].getValue();
+                sensorarray[i][0] = getNewSensorValue(sensors[i].getValue());
             }
-
-            double aktorenarray[][] = {{10, 5, 20}, {20, 5, 10}};
+            double aktorenarray[][] = {{1, 0.3, 0}, {0, 0.7, 1}};
             double [][] result =  multiplyMatrix(aktorenarray, sensorarray);
 
-            //driveRight();
+            double rightSpeed = getSpeedForWheel(result, 1);
+            double leftSpeed = getSpeedForWheel(result, 0);
+
+            setSpeed(leftSpeed + 500, rightSpeed + 500);
         }
+    }
+
+    private double getNewSensorValue(double value) {
+        double newValue = (value * 100) / 4200;
+        newValue = 100 - newValue;
+        return  newValue;
+    }
+
+    private double getSpeedForWheel(double[][] result, int i) {
+        double value = 0.0;
+        for(int j = 0; j < result[i].length; j++) {
+            value += result[i][j];
+        }
+        return value;
     }
 
     private double [][] multiplyMatrix(double firstarray[][], double secondarray[][]) {
         /* Create another array to store the result using the original arrays' lengths on row and column respectively. */
         double [][] result = new double[firstarray.length][secondarray[0].length];
-
         for (int i = 0; i < firstarray.length; i++) {
             for (int j = 0; j < secondarray[0].length; j++) {
                 for (int k = 0; k < firstarray[0].length; k++) {
