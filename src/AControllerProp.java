a9f340c2abd7c052c1beef2d0c0236e1f061493c
tscholy <christina.tschol@students.fhv.at>
2017-12-05 16:05:39 +0100

    changed controller

--- src/AControllerProp.java
+++ src/AControllerProp.java
@@ -2,10 +2,17 @@ import com.cyberbotics.webots.controller.DifferentialWheels;
 import com.cyberbotics.webots.controller.DistanceSensor;
 import com.cyberbotics.webots.controller.LightSensor;
 
-public class AControllerProp extends ControllerProp {
+public class AControllerProp extends DifferentialWheels {
+
+    private static int TIME_STEP = 15;
+    private LightSensor[] sensors;
 
     public AControllerProp() {
         super();
+        sensors = new LightSensor[] { getLightSensor("ls1"), getLightSensor("ls4"), getLightSensor("ls6") };
+        for (int i=0; i<3; i++) {
+            sensors[i].enable(10);
+        }
     }
 
     public static void main(String[] args) {
@@ -27,5 +34,32 @@ public class AControllerProp extends ControllerProp {
 
             setSpeed(leftSpeed + 500, rightSpeed + 500);
         }
+    }
+
+    private double getNewSensorValue(double value) {
+        double newValue = (value * 100) / 4200;
+        newValue = 100 - newValue;
+        return  newValue;
+    }
+
+    private double getSpeedForWheel(double[][] result, int i) {
+        double value = 0.0;
+        for(int j = 0; j < result[i].length; j++) {
+            value += result[i][j];
+        }
+        return value;
+    }
+
+    private double [][] multiplyMatrix(double firstarray[][], double secondarray[][]) {
+        /* Create another array to store the result using the original arrays' lengths on row and column respectively. */
+        double [][] result = new double[firstarray.length][secondarray[0].length];
+        for (int i = 0; i < firstarray.length; i++) {
+            for (int j = 0; j < secondarray[0].length; j++) {
+                for (int k = 0; k < firstarray[0].length; k++) {
+                    result[i][j] += firstarray[i][k] * secondarray[k][j];
+                }
+            }
+        }
+        return result;
     }
 }
--- src/BControllerProp.java
+++ src/BControllerProp.java
@@ -1,10 +1,17 @@
 import com.cyberbotics.webots.controller.DifferentialWheels;
 import com.cyberbotics.webots.controller.LightSensor;
 
-public class BControllerProp extends ControllerProp {
+public class BControllerProp extends DifferentialWheels {
+
+    protected static int TIME_STEP = 15;
+    protected LightSensor[] sensors;
 
     public BControllerProp() {
         super();
+        sensors = new LightSensor[] { getLightSensor("ls1"), getLightSensor("ls4"), getLightSensor("ls6") };
+        for (int i=0; i<3; i++) {
+            sensors[i].enable(10);
+        }
     }
 
     public static void main(String[] args) {
@@ -16,8 +23,9 @@ public class BControllerProp extends ControllerProp {
             double sensorarray[][] = new double[3][1];
             boolean inFrontOfLight = false;
             for(int i = 0; i < sensors.length; i++) {
-                if(sensors[i].getValue() <= 1 ) {
+                if(sensors[i].getValue() <= 1.0 ) {
                     inFrontOfLight = true;
+                    break;
                 }
                 sensorarray[i][0] = getNewSensorValue(sensors[i].getValue());
             }
@@ -33,6 +41,33 @@ public class BControllerProp extends ControllerProp {
                 break;
             }
         }
+    }
+
+    private double getNewSensorValue(double value) {
+        double newValue = (value * 100) / 4200;
+        newValue = 100 - newValue;
+        return  newValue;
+    }
+
+    private double getSpeedForWheel(double[][] result, int i) {
+        double value = 0.0;
+        for(int j = 0; j < result[i].length; j++) {
+            value += result[i][j];
+        }
+        return value;
+    }
+
+    private double [][] multiplyMatrix(double firstarray[][], double secondarray[][]) {
+        /* Create another array to store the result using the original arrays' lengths on row and column respectively. */
+        double [][] result = new double[firstarray.length][secondarray[0].length];
+        for (int i = 0; i < firstarray.length; i++) {
+            for (int j = 0; j < secondarray[0].length; j++) {
+                for (int k = 0; k < firstarray[0].length; k++) {
+                    result[i][j] += firstarray[i][k] * secondarray[k][j];
+                }
+            }
+        }
+        return result;
     }
 
 }
